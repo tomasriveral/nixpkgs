df2fcddd742d05749aa0d5b55d7d7ce1d5863220
Robert Schütz <nix@dotlambda.de>
2026-03-29 23:02:34 -0700

    python3Packages.infrared-protocols: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7527,6 +7527,8 @@ self: super: with self; {
 
   inform = callPackage ../development/python-modules/inform { };
 
+  infrared-protocols = callPackage ../development/python-modules/infrared-protocols { };
+
   ingredient-parser-nlp = callPackage ../development/python-modules/ingredient-parser-nlp { };
 
   iniconfig = callPackage ../development/python-modules/iniconfig { };
